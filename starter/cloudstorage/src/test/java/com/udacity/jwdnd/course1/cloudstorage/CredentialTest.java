762e9f64f9b4d0a7bf29561c9050709ae27c9460
Mshari <32165999+MshariAlsayari@users.noreply.github.com>
2022-03-03 12:59:05 +0300

    - implement all credentials unit test

---
+++ starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CredentialTest.java
@@ -0,0 +1,94 @@
+package com.udacity.jwdnd.course1.cloudstorage;
+
+
+import com.udacity.jwdnd.course1.cloudstorage.model.Credential;
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CredentialTest  extends CloudStorageApplicationTests {
+
+
+    @Test
+    public void teat_create_credential() {
+        String credentialUrl = "first credential";
+        String credentialUsername = "this is my first credential.";
+        HomePage homePage = doRegistrationAndLogin();
+        initCredential(credentialUrl, credentialUsername, homePage);
+        homePage = new HomePage(driver);
+        Credential credential = homePage.getCredential();
+        Assertions.assertEquals(credentialUrl, credential.getUrl());
+        Assertions.assertEquals(credentialUsername, credential.getUserName());
+    }
+
+    @Test
+    public void teat_delete_credential() {
+        String credentialUrl = "first credential";
+        String credentialUsername = "this is my first credential.";
+        HomePage homePage = doRegistrationAndLogin();
+        initCredential(credentialUrl, credentialUsername, homePage);
+        homePage = new HomePage(driver);
+        deleteCredential(homePage);
+        Credential credential = homePage.getExampleCredential();
+        Assertions.assertEquals("Example Credential URL", credential.getUrl());
+        Assertions.assertEquals("Example Credential Username", credential.getUserName());
+    }
+
+    @Test
+    public void teat_edit_note() {
+        String credentialUrl = "first credential";
+        String credentialUsername = "this is my first credential.";
+        String updatedCredentialUrl = "updated credential";
+        String updatedCredentialUsername = "my first updated credential.";
+        HomePage homePage = doRegistrationAndLogin();
+        initCredential(credentialUrl, credentialUsername, homePage);
+        homePage = new HomePage(driver);
+        editCredential(updatedCredentialUrl, updatedCredentialUsername,homePage);
+        Credential credential = homePage.getCredential();
+        Assertions.assertEquals(updatedCredentialUrl, credential.getUrl());
+        Assertions.assertEquals(updatedCredentialUsername, credential.getUserName());
+    }
+
+
+
+
+
+
+
+
+
+
+    private void initCredential(String url, String username, HomePage page){
+        page.navTo(HomePage.Tabs.CREDENTIALS);
+        page.clickOnAddCredentialButton();
+        page.setCredentialUrl(url);
+        page.setCredentialUsername(username);
+        page.setCredentialPassword("123");
+        page.clickOnSubmitCredentialButton();
+        ResultPage resultPage = new ResultPage(driver);
+        resultPage.clickBtnSuccessResult();
+        page.navTo(HomePage.Tabs.CREDENTIALS);
+    }
+
+    private void deleteCredential(HomePage page){
+        page.navTo(HomePage.Tabs.CREDENTIALS);
+        page.clickOnDeleteCredentialButton();
+        ResultPage resultPage = new ResultPage(driver);
+        resultPage.clickBtnSuccessResult();
+        page.navTo(HomePage.Tabs.CREDENTIALS);
+    }
+
+    private void editCredential(String url, String username, HomePage page){
+        page.navTo(HomePage.Tabs.CREDENTIALS);
+        page.clickOnEditCredentialButton();
+        page.setCredentialUrl(url);
+        page.setCredentialUsername(username);
+        page.setCredentialPassword("123");
+        page.clickOnSubmitCredentialButton();
+        ResultPage resultPage = new ResultPage(driver);
+        resultPage.clickBtnSuccessResult();
+        page.navTo(HomePage.Tabs.CREDENTIALS);
+    }
+}
--- starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/HomePage.java
+++ starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/HomePage.java
@@ -1,6 +1,7 @@
 package com.udacity.jwdnd.course1.cloudstorage;
 
 
+import com.udacity.jwdnd.course1.cloudstorage.model.Credential;
 import com.udacity.jwdnd.course1.cloudstorage.model.Note;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -47,12 +48,54 @@ public class HomePage {
     @FindBy(id = "tableNoteDescription")
     private WebElement noteDescription;
 
-
     @FindBy(id = "exampleNoteTitle")
     private WebElement exampleNoteTitle;
 
     @FindBy(id = "exampleNoteDescription")
     private WebElement exampleNoteDescription;
+
+
+
+
+    // Credential
+    @FindBy(id = "btn-add-credential")
+    private WebElement btnAddCredential;
+
+    @FindBy(id = "btn-edit-credential")
+    private WebElement btnEditCredential;
+
+    @FindBy(id = "btn-delete-credential")
+    private WebElement btnDeleteCredential;
+
+    @FindBy(id = "credential-url")
+    private WebElement editTextCredentialUrl;
+
+    @FindBy(id = "credential-username")
+    private WebElement editTextCredentialUsername;
+
+    @FindBy(id = "credential-password")
+    private WebElement editTextCredentialPassword;
+
+    @FindBy(id = "credentialSubmit")
+    private WebElement btnSubmitCredential;
+
+    @FindBy(id = "tableCredentialUrl")
+    private WebElement credentialUrl;
+
+    @FindBy(id = "tableCredentialUsername")
+    private WebElement credentialUsername;
+
+    @FindBy(id = "tableCredentialPassword")
+    private WebElement credentialPassword;
+
+    @FindBy(id = "exampleCredentialUrl")
+    private WebElement exampleCredentialUrl;
+
+    @FindBy(id = "exampleCredentialUsername")
+    private WebElement exampleCredentialUsername;
+
+    @FindBy(id = "exampleCredentialPassword")
+    private WebElement exampleCredentialPassword;
 
 
 
@@ -84,6 +127,7 @@ public class HomePage {
 
     }
 
+    //Note
     public void clickOnAddNoteButton() {
         js.executeScript("arguments[0].click();", btnAddNote);
     }
@@ -109,6 +153,37 @@ public class HomePage {
     }
 
 
+
+    //Credential
+    public void clickOnAddCredentialButton() {
+        js.executeScript("arguments[0].click();", btnAddCredential);
+    }
+
+    public void setCredentialUrl(String url) {
+        js.executeScript("arguments[0].value='" + url + "';", editTextCredentialUrl);
+    }
+
+    public void setCredentialUsername(String username) {
+        js.executeScript("arguments[0].value='" + username + "';", editTextCredentialUsername);
+    }
+
+    public void setCredentialPassword(String password) {
+        js.executeScript("arguments[0].value='" + password + "';", editTextCredentialPassword);
+    }
+
+    public void clickOnSubmitCredentialButton() {
+        js.executeScript("arguments[0].click();", btnSubmitCredential);
+    }
+
+    public void clickOnDeleteCredentialButton() {
+        js.executeScript("arguments[0].click();", btnDeleteCredential);
+    }
+
+    public void clickOnEditCredentialButton() {
+        js.executeScript("arguments[0].click();", btnEditCredential);
+    }
+
+
     public Note getNote() {
         String title = wait.until(ExpectedConditions.elementToBeClickable(noteTitle)).getText();
         String description = noteDescription.getText();
@@ -119,6 +194,18 @@ public class HomePage {
         String title = wait.until(ExpectedConditions.elementToBeClickable(exampleNoteTitle)).getText();
         String description = exampleNoteDescription.getText();
         return new Note(title, description);
+    }
+
+    public Credential getCredential() {
+        String url = wait.until(ExpectedConditions.elementToBeClickable(credentialUrl)).getText();
+        String username =wait.until(ExpectedConditions.elementToBeClickable(credentialUsername)).getText();
+        return new Credential(url, username,"");
+    }
+
+    public Credential getExampleCredential() {
+        String url = wait.until(ExpectedConditions.elementToBeClickable(exampleCredentialUrl)).getText();
+        String username =wait.until(ExpectedConditions.elementToBeClickable(exampleCredentialUsername)).getText();
+        return new Credential(url, username, "");
     }
 
 
